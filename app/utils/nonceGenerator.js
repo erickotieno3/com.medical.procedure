70be5daa9a82b2170f173716af83b2f545edc256
Erick Otieno <erickotieno3@yahoo.com>
2026-03-15 09:31:04 +0000

    FIX: Add nonceGenerator.js to app/utils/ to resolve import error

---
+++ app/utils/nonceGenerator.js
@@ -0,0 +1,11 @@
+﻿// utils/nonceGenerator.js
+import * as Crypto from 'expo-crypto';
+
+export const generateNonce = async () => {
+  const nonce = Crypto.randomUUID();
+  const hashedNonce = await Crypto.digestStringAsync(
+    Crypto.CryptoDigestAlgorithm.SHA256,
+    nonce
+  );
+  return { nonce, hashedNonce };
+};
